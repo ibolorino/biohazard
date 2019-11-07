16928f8f5bd4d8c051ee78672335c429ddd1038c
Ivan Bolorino <ivan.bolorino@gmail.com>
2019-11-07 15:48:47 -0300

    add get_solutions_index()

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 from random import random
 
 
-def start_samples(order, max_samples):
+def start_samples(affected_pairs, max_samples):
     min = 1
     max = max_samples
     sample = []
-    for _ in range(0, order):
+    for _ in range(0, affected_pairs):
         sample.append(scaled_random(min, max))
     return sample
 
@@ -52,3 +52,52 @@ def delete_useless_values(list1, list2):
         del (list1[val - i])
         del (list2[val - i])
     return list1, list2
+
+
+def get_all_solutions(max_samples):
+    solution = []
+    all_solutions = []
+    for k in range(1, max_samples+1):
+        for i in range(k, max_samples+1):
+            solution.clear()
+            for j in range (k, i+1):
+                solution.append(j)
+                #print ("(%s, %s)" %(i,j))
+            all_solutions.append(list(solution))
+    return all_solutions
+
+
+def get_range(num, max_samples):
+    min_range = num - 1
+    num = max_samples - num
+    max_range = 0
+    for i in range(max_samples, num, -1):
+        max_range += i
+    max_range -= 1
+    return min_range, max_range
+
+
+def get_solutions_index(num, max_samples):
+    solutions_index = []
+    for i in range(1, num+1):
+        block_range = get_block_range(i, max_samples)
+        start_range = block_range[0] + num - i
+        stop_range = block_range[1]
+        solutions_index.append([start_range, stop_range])
+    return solutions_index
+
+
+def get_block_range(num, max_samples):
+    sum_max_samples = sum_all(max_samples)
+    start_range = sum_max_samples - sum_all(max_samples - num + 1)
+    stop_range = sum_max_samples - sum_all(max_samples - num) - 1
+    return [start_range, stop_range]
+
+
+def sum_all(num):
+    sum = (num+1)*num/2
+    return int(sum)
+
+
+def get_num_coexisting_samples(all_solutions, min_list, max_list):
+    return
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from functions import start_samples, delete_same_values, set_max_min_lists, delete_useless_values
+from functions import start_samples, delete_same_values, set_max_min_lists, delete_useless_values, get_all_solutions, \
+    get_solutions_index
 
-max_samples = 15
-affected_pairs = 10
+max_samples = 5
+affected_pairs = 1
 affected = start_samples(affected_pairs, max_samples)
 poisonous = start_samples(affected_pairs, max_samples)
 poisonous = delete_same_values(affected, poisonous, max_samples)
 min_list, max_list = set_max_min_lists(affected, poisonous)
-print(min_list)
-print(max_list)
 min_list, max_list = delete_useless_values(min_list, max_list)
-print(min_list)
-print(max_list)
+
+all_solutions = get_all_solutions(max_samples)
+
+for i in range(0, len(all_solutions)):
+   print("%s - %s" %(i, all_solutions[i]))
+
+ranges_index = get_solutions_index(2, max_samples)
+print(ranges_index)
+
 
